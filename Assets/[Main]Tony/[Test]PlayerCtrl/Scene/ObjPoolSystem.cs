a0cf65b33e963079edf4a174e3b1dab43984413a
Tony <tony7758623@gmail.com>
2024-02-12 16:20:10 +0800

    assets整理

--- Assets/[Main]Tony/[Test]PlayerCtrl/Scene/ObjPoolSystem.cs
+++ Assets/[Main]Tony/[Test]PlayerCtrl/Scene/ObjPoolSystem.cs
@@ -6,11 +6,9 @@ using Zenject;
 public class ObjPoolSystem : MonoInstaller {
     
     public GameObject HealthBarPrefab;
-    //public GameObject ButtetPrefab;
     
     public override void InstallBindings() {
         Container.Bind<ObjPoolCtrl<HealthBarCtrl>>().FromInstance(new ObjPoolCtrl<HealthBarCtrl>(HealthBarPrefab,null)).AsSingle();
-        //Container.Bind<ObjPoolCtrl<BulletCtrl>>().FromInstance(new ObjPoolCtrl<BulletCtrl>(ButtetPrefab,null)).AsSingle();
     }
     
 }
