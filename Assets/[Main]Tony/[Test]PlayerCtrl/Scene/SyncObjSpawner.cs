8305aee87b9ddeecac37b0d8cd139de34bca5065
ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2024-02-13 15:02:15 +0800

    added bullet collision detect

--- Assets/[Main]Tony/[Test]PlayerCtrl/Scene/SyncObjSpawner.cs
+++ Assets/[Main]Tony/[Test]PlayerCtrl/Scene/SyncObjSpawner.cs
@@ -1,3 +1,5 @@
+using UniRx;
+using UniRx.Triggers;
 using Unity.Netcode;
 using UnityEngine;
 using Zenject;
@@ -11,10 +13,29 @@ public class SyncObjSpawner : NetworkBehaviour{
 	public GameObject ButtetPrefab;
 
 	[ServerRpc(RequireOwnership = false)]
-	public void SpawnBulletServerRpc(Vector2 genPos, float angle, float moveSec, float maxDis){
-		var bulletClone = Instantiate(ButtetPrefab, genPos, Quaternion.Euler(0, 0, angle)).GetComponent<NetworkObject>();
+	public void SpawnBulletServerRpc(Vector2 genPos, float angle, float moveSec, float maxDis,
+		ulong playerId = default){
+		var bulletClone = Instantiate(ButtetPrefab, genPos, Quaternion.Euler(0, 0, angle))
+				.GetComponent<NetworkObject>();
 		bulletClone.Spawn();
-		bulletClone.GetComponent<BulletCtrl>().Setup(genPos,angle, moveSec, maxDis, () => { bulletClone.Despawn();});
+		bulletClone.GetComponent<BulletCtrl>().Setup(genPos, angle, moveSec, maxDis, () => { bulletClone.Despawn(); });
+		bulletClone.OnCollisionEnterAsObservable().Subscribe(x => OnBulletHit(x, playerId));
 	}
 
+	//Server Only
+	private void OnBulletHit(Collision obj, ulong playerId){
+		if(!obj.transform.root.TryGetComponent<PlayerCtrl>(out var hitPlayer)){
+			return;
+		}
+
+		var hitPlayerId = hitPlayer.GetComponent<NetworkObject>().OwnerClientId;
+		if(playerId != hitPlayerId){
+			PlayerHitRequestServerRpc(playerId, hitPlayerId, 5);
+		}
+	}
+
+	[ServerRpc(RequireOwnership = false)]
+	public void PlayerHitRequestServerRpc(ulong attackerId, ulong hitId, int damage){
+		Debug.Log($"Receive Hit Request {hitId} by {attackerId} ");
+	}
 }
