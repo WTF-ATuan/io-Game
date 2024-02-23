d6337c2d69c28acdf300b6b9685c0cf9a059ddc3
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2024-02-23 14:33:37 +0800

    fixed match making prototype

--- Assets/MatchMaking Prototype/ClientStatusUI.cs
+++ Assets/MatchMaking Prototype/ClientStatusUI.cs
@@ -25,20 +25,7 @@ namespace Assets.Scenes{
 		public void OnConnect(){
 			connectingText.gameObject.SetActive(false);
 			playerList.transform.parent.gameObject.SetActive(true);
-			PlayerIDListRequestServerRPC();
+			playerList.text = $"Player ID : {NetworkManager.Singleton.LocalClientId} \n";
 		}
-
-		//Server 還沒有這段的版本Build 所以還不能觸發
-		[ServerRpc(RequireOwnership = false)]
-		private void PlayerIDListRequestServerRPC(){
-			var connectedClients = NetworkManager.Singleton.ConnectedClients;
-			foreach(var client in connectedClients){
-				var clientID = client.Key;
-				playerList.text += $"Player ID : {clientID} \n";
-			}
-		}
-
-
-
 	}
 }
