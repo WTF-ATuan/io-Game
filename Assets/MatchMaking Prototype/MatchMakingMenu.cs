fd421f20fe3c4ad4dcf085451ad41024b2c95c29
WTF-ATuan <62327517+WTF-ATuan@users.noreply.github.com>
2024-03-16 14:49:01 +0800

    rebuild matchmaking feature and make it work

---
+++ Assets/MatchMaking Prototype/MatchMakingMenu.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scenes{
+	public class MatchMakingMenu : MonoBehaviour{
+		private ClientGameManager _clientGameManager;
+
+		[SerializeField] private Button matchButton;
+		[SerializeField] private TMP_Text connectingText;
+		[SerializeField] private TMP_Text playerIdText;
+
+
+		private void Start(){
+			_clientGameManager = ClientSingleton.Instance.Manager;
+		}
+
+		public async void StartMatch(){
+			matchButton.gameObject.SetActive(false);
+			connectingText.gameObject.SetActive(true);
+			connectingText.text = "Connecting...";
+			await _clientGameManager.MatchmakeAsync(OnMatchMade);
+		}
+		
+		private void OnMatchMade(MatchmakerPollingResult result){
+			connectingText.gameObject.SetActive(false);
+			playerIdText.transform.parent.gameObject.SetActive(true);
+			playerIdText.text = $"Player ID : {_clientGameManager.User.AuthId}";
+		}
+	}
+}
