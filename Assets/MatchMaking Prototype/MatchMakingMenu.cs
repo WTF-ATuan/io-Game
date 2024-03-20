66bc8799d7a54b81295963b206250839c82d4a2c
Atuan <jain60501@gmail.com.tw>
2024-03-20 14:40:59 +0800

    fixed menu bug

--- Assets/MatchMaking Prototype/MatchMakingMenu.cs
+++ Assets/MatchMaking Prototype/MatchMakingMenu.cs
@@ -4,29 +4,22 @@ using UnityEngine.UI;
 
 namespace Assets.Scenes{
 	public class MatchMakingMenu : MonoBehaviour{
-		private ClientGameManager _clientGameManager;
-
 		[SerializeField] private Button matchButton;
 		[SerializeField] private TMP_Text connectingText;
 		[SerializeField] private TMP_Text playerIdText;
-
-
-		private void Start(){
-			if(_clientGameManager == null) return;
-			_clientGameManager = ClientSingleton.Instance.Manager;
-		}
+		
 
 		public async void StartMatch(){
 			matchButton.gameObject.SetActive(false);
 			connectingText.gameObject.SetActive(true);
 			connectingText.text = "Connecting...";
-			await _clientGameManager.MatchmakeAsync(OnMatchMade);
+			await ClientSingleton.Instance.Manager.MatchmakeAsync(OnMatchMade);
 		}
 
 		private void OnMatchMade(MatchmakerPollingResult result){
 			connectingText.gameObject.SetActive(false);
 			playerIdText.transform.parent.gameObject.SetActive(true);
-			playerIdText.text = $"Player ID : {_clientGameManager.User.AuthId}";
+			playerIdText.text = $"Player ID : {ClientSingleton.Instance.Manager.User.AuthId}";
 		}
 	}
 }
