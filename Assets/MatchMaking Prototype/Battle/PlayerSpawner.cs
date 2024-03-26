9a3d264b4aeebc3d15836aff12807bb76abb5b39
Atuan <jain60501@gmail.com.tw>
2024-03-26 16:33:33 +0800

    Turn scene context to project context fixed inject bug

--- Assets/MatchMaking Prototype/Battle/PlayerSpawner.cs
+++ Assets/MatchMaking Prototype/Battle/PlayerSpawner.cs
@@ -6,20 +6,24 @@ namespace MatchMaking_Prototype.Battle{
 	public class PlayerSpawner : NetworkBehaviour{
 		[SerializeField] private NetworkObject playerPrefab;
 
-		public override void OnNetworkSpawn()
-		{
-			if (!IsServer) { return; }
-			
+		private void Start(){
+			//TestCreate();
+		}
+
+		public override void OnNetworkSpawn(){
+			if(!IsServer){
+				return;
+			}
+
 			foreach(var client in NetworkManager.Singleton.ConnectedClients){
-					var spawnPos = Vector3.zero;
-					var characterInstance = Instantiate(playerPrefab, spawnPos, Quaternion.identity);
-					characterInstance.SpawnAsPlayerObject(client.Key);
+				var spawnPos = Vector3.zero;
+				var characterInstance = Instantiate(playerPrefab, spawnPos, Quaternion.identity);
+				characterInstance.SpawnAsPlayerObject(client.Key);
 			}
 		}
 
 		public void TestCreate(){
-			Instantiate(playerPrefab.gameObject, Vector3.zero, Quaternion.identity);
+			Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
 		}
-
 	}
 }
